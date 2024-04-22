ecdd253b997d4ab63f4b843d1eab561604b6bfce
milicanina <migrutinovic13@gmail.com>
2024-04-22 23:48:29 +0200

    Tanker update

--- src/Tanker.js
+++ src/Tanker.js
@@ -6,8 +6,8 @@ const Tanker = ({ tankerNumber, initialName }) => {
     const [tankerName, setTankerName] = useState(
         localStorage.getItem(`tankerName${tankerNumber}`) || initialName
     );
-    const [input, setInput] = useState(Array.from({ length: 15 }).map(() => 0)); //inicijalizujem input i output 
-    const [output, setOutput] = useState(Array.from({ length: 15 }).map(() => 0));
+    const [input, setInput] = useState(Array.from({ length: 20 }).map(() => 0)); //inicijalizujem input i output 
+    const [output, setOutput] = useState(Array.from({ length: 20 }).map(() => 0));
 
     const handleNameChange = () => {
         const newName = prompt('Unesite novo ime tankera:', tankerName);
@@ -22,7 +22,7 @@ const Tanker = ({ tankerNumber, initialName }) => {
         const sumOutputs = output.reduce((acc, curr) => acc + parseFloat(curr), 0);
         return sumInputs - sumOutputs;
     };
-    
+
     useEffect(() => {
         const sumElement = document.getElementById(`ukupno-${tankerNumber}`);
         if (sumElement) {
@@ -35,7 +35,7 @@ const Tanker = ({ tankerNumber, initialName }) => {
     }, [tankerNumber, tankerName]);
     return (
         <div>
-            
+
             <div className='background'>
                 <div className='flex space-x-4'>
                     <div className='one-card'>
@@ -51,7 +51,7 @@ const Tanker = ({ tankerNumber, initialName }) => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {Array.from({ length: 15 }).map((_, index) => (
+                                    {Array.from({ length: 20 }).map((_, index) => (
                                         <tr className='tr' key={index}>
                                             <td className='tr'>
                                                 <input type="number" onChange={(e) => {
@@ -86,9 +86,16 @@ const Tanker = ({ tankerNumber, initialName }) => {
                     </div>
 
                     <div className='one-card'>
-                        <div className='row'>
-                        <div className='sum'>UKUPNO (L):<span id={`ukupno-${tankerNumber}`}>0</span></div>
+                        <div className='flex'>
+                            <div className='sum w-96'>UKUPNO (L):<span id={`ukupno-${tankerNumber}`}>0</span></div>
                            
+                           <div className='report '>
+                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                            </svg>
+                           </div>
+
+
 
                         </div>
                         <div>TANKER PICTURE</div>
